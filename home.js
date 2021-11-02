545c0caf567dc63b0fe3edde57c6320c1144472f
Saijeet7 <np03s200062@heraldcollege.edu.np>
2021-11-02 20:36:58 +0545

    About Numbers

---
+++ home.js
@@ -0,0 +1,28 @@
+console.log("hello");
+
+//Variables
+var b = 'smoothie';
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+
+// var age = prompt('What is your age?');
+// document.getElementById('someText').innerHTML= age;
+
+//Number in JavaScript
+
+var num1 = 10;
+//Increment num1 by 1
+num1++;
+//Decrement num1 by 1
+num1--;
+console.log(num1);
+
+//Divide, multiply*, remainder%
+console.log(num1%6);
+
+//Increment/decrement by any number you want
+num1 += 30;
+console.log(num1);
