5b205e2862edf70b6dac159535685a5ad5ac4210
Saijeet7 <np03s200062@heraldcollege.edu.np>
2021-11-02 21:43:20 +0545

    About arrays

--- home.js
+++ home.js
@@ -52,8 +52,8 @@ function greeting(yourName) {
     console.log(result);
 }
 
-var name = prompt('What is your name?');
-greeting(name);
+//var name = prompt('What is your name?');
+//greeting(name);
 
 //How do arguments work on function?
 // Add 2 numbers in a function
@@ -62,4 +62,87 @@ function sumNumber(num1,num2) {
     var result = num1 + num2;
     console.log(num1+num2);
 }
+
 sumNumber(10,10);
+
+//While Loops
+/*var num = 0;
+ while (num <100){
+     num +=1;
+     console.log(num);
+ }
+
+
+*/
+// For Loop
+/*for (let num =0; num<=100; num++){
+    console.log(num);
+}*/
+
+// Data Types
+let yourAge =18;                                //number
+let yourName = 'Bob';                           //String
+let name = {first: 'Jane', last: 'Doe'};        //object
+let truth = false;                              //boolean
+let groceries = ['apple','banana','oranges'];   //array
+let random;                                     //undefined
+let nothing = null                              //value null
+
+
+//Strings in JavaScript (common methods)
+let fruit = 'banana,apple,orange,blackberry';
+let moreFruits = 'banana\napple';               //new line
+
+console.log(moreFruits);
+console.log(fruit.length);
+console.log(fruit.indexOf('nan'));
+console.log(fruit.slice(2, 6));
+console.log(fruit.replace('ban','123'));
+console.log(fruit.toUpperCase(fruit));
+console.log(fruit.split(','));              //split by a comma
+console.log(fruit.split(''));               //split by character
+
+//Arrays
+let fruits= ['banana','apple','orange','blackberry'];
+fruits = new Array('banana','apple','orange','blackberry');
+
+console.log(fruits[0]); //access value at index 2nd
+
+fruits[0] = 'pear';
+console.log(fruits); 
+
+for (let i=0; i<fruits.length; i++){
+    console.log(fruits[i]);
+}
+
+//Array common methods
+console.log('to String:',fruits.toString());
+console.log(fruits.join(' * '));
+console.log(fruits.pop(),fruits); // removes last item
+console.log(fruits.push('blackberry'),fruits); // appends
+console.log(fruits[4]);
+fruits[fruits.length] = 'new fruit'; //same as push
+console.log(fruits);
+fruits.shift(); //remove first element from a list
+console.log(fruits);
+fruits.unshift('kiwi'); // add first element to an array
+console.log(fruits);
+
+let vegetables = ['asparagus','tomato','broccoli'];
+let allGroceries = fruits.concat(vegetables); //combine arrays
+console.log(allGroceries);
+console.log(allGroceries.slice(1,4));
+console.log(allGroceries.reverse());
+console.log(allGroceries.sort());
+
+let someNumbers = [ 5,12 ,1 ,211 ,3 ,35 ,345 ,46 ,12 , 6];
+console.log(someNumbers.sort(function(a,b){return a-b})); //sorted in ascending order
+console.log(someNumbers.sort(function(a,b){return b-a})); //sorted in decending order
+
+let emptyArray = new Array();
+
+for (let num=0; num<=10; num++){
+    emptyArray.push(num);
+}
+
+console.log(emptyArray);
