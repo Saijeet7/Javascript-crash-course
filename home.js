8dcfba3bfd498a2945be9f4e7465fccd75299990
Saijeet7 <np03s200062@heraldcollege.edu.np>
2021-11-02 20:51:57 +0545

    About Functions

--- home.js
+++ home.js
@@ -26,3 +26,40 @@ console.log(num1%6);
 //Increment/decrement by any number you want
 num1 += 30;
 console.log(num1);
+
+//Functions
+/*
+1. Create a function
+2. Call the function
+*/
+
+function fun() {
+    console.log('This is a function');
+}
+//Call
+fun();
+/* Let's create a function that take in a name and says
+hello followed by your name
+
+For Example
+Name: "Saijeet"
+Return: "Hello Saijeet"
+*/
+
+
+function greeting(yourName) {
+    var result = 'Hello'+' '+ yourName; // String Concatenation
+    console.log(result);
+}
+
+var name = prompt('What is your name?');
+greeting(name);
+
+//How do arguments work on function?
+// Add 2 numbers in a function
+
+function sumNumber(num1,num2) {
+    var result = num1 + num2;
+    console.log(num1+num2);
+}
+sumNumber(10,10);
