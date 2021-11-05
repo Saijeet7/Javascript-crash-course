7467303eed5fb17fafe6da7734d493af7c0db07c
Saijeet7 <np03s200062@heraldcollege.edu.np>
2021-11-05 19:03:11 +0545

    Challenge 4 JS

--- Challenge1/script.js
+++ Challenge1/script.js
@@ -89,3 +89,57 @@ function rpsFrontEnd(humanImgChoice,computerImgChoice, finalMessage){
     document.getElementById('flex-box-rps-div').appendChild(messageDiv);
     document.getElementById('flex-box-rps-div').appendChild(botDiv);
 }
+
+
+//Challenge 4 : Change the Color of All buttons
+var all_buttons = document.getElementsByTagName('button');
+console.log(all_buttons);
+
+var copyAllButtons = [];
+for (let i =0; i <all_buttons.length; i++){
+    copyAllButtons.push(all_buttons[i].classList[1]);
+}
+
+console.log(copyAllButtons);
+
+function buttonColorChange(buttonThing){
+    if (buttonThing.value ==="red"){
+        buttonRed();
+    }else if (buttonThing.value ==="green"){
+        buttonGreen();
+    }else if (buttonThing.value ==="reset"){
+        buttonColorReset();
+    }else if (buttonThing.value ==="random"){
+        randomColor();
+    }
+}
+
+function buttonRed(){
+    for (let i = 0; i < all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-danger');
+    }
+}
+
+function buttonGreen(){
+    for (let i = 0; i < all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-success');
+    }
+}
+
+function buttonColorReset(){
+    for (let i=0; i<all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(copyAllButtons[i]);
+    }
+}
+
+function randomColor(){
+    var choices = ['btn-primary','btn-warning','btn-success','btn-danger']
+    for (let i = 0; i < all_buttons.length; i++){
+        var randomNumber = Math.floor(Math.random()*4);
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(choices[randomNumber]);
+    }
+}
